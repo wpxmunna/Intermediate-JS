8999d3bb3d92ee53168c9de201397d08a167c315
wpxmunna <wpxmunna@gmail.com>
2022-05-31 18:23:34 +0600

    Undefined Vs Null

---
+++ undefined-null.js
@@ -0,0 +1,33 @@
+// Undefined---
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+// }
+// const result = add(12, 13);
+// console.log(result);
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+//     return
+// }
+// const result = add(12, 13);
+// console.log(result);
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(15);
+// console.log(result);
+
+// const premika = {name: 'Smart', number: 123456};
+// console.log(premika.gf);
+
+// ---same for array ---
+
+let fun = undefined;
+console.log(fun);
+
+const number = null;
+console.log(number);
